1148d6b4541a3731494c1d2d208e5ce8ee2964e2
Jerome Magpantay <jerome.magpantay247@gmail.com>
2024-11-23 16:59:53 -0500

    cors + new sample data

--- Backend/venv/Scripts/activate.bat
+++ Backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Jerome\Desktop\CPS714\backend\venv
+set VIRTUAL_ENV=C:\Users\jerom\Desktop\CPSPROJECT\BizPoints-Customer-Support-Ticketing-System\Backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
